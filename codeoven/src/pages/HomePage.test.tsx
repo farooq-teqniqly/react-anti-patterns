6f3275cae90ce3da3731534f8bf189a27e94a439
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-01-13 16:59:55 -0800

    Add items to cart

--- codeoven/src/pages/HomePage.test.tsx
+++ codeoven/src/pages/HomePage.test.tsx
@@ -1,10 +1,12 @@
 import { render, screen, within } from "@testing-library/react";
 import { HomePage } from "./HomePage";
 import { foodMenu } from "../data/MenuItems";
+import userEvent from "@testing-library/user-event";
 
 describe("Home Page", () => {
   it("Renders the heading", () => {
     render(<HomePage></HomePage>);
+
     const heading = screen.getByText("The Code Oven");
     expect(heading).toBeInTheDocument();
   });
@@ -12,7 +14,7 @@ describe("Home Page", () => {
   it("Renders the food menu", () => {
     render(<HomePage></HomePage>);
 
-    const menuList = screen.getByRole("list");
+    const menuList = screen.getByTestId("menuList");
     const menuItems = within(menuList).getAllByRole("listitem");
     expect(menuItems.length).toEqual(foodMenu.length);
 
@@ -20,5 +22,48 @@ describe("Home Page", () => {
     foodMenu.map((p, i) => {
       expect(within(menuItems[i]).getByText(p)).toBeInTheDocument();
     });
+  });
+
+  it("Renders the cart", () => {
+    render(<HomePage></HomePage>);
+
+    const cart = screen.getByTestId("cart");
+    expect(cart).toBeInTheDocument();
+
+    const placeOrderButton = within(cart).getByRole("button");
+    expect(placeOrderButton).toBeInTheDocument();
+    expect(placeOrderButton).toHaveTextContent("Place Order");
+    expect(placeOrderButton).toBeDisabled();
+  });
+
+  it("Renders an add to order button for each menu item", () => {
+    render(<HomePage></HomePage>);
+
+    const menuList = screen.getByTestId("menuList");
+    const menuItems = within(menuList).getAllByRole("listitem");
+
+    // eslint-disable-next-line array-callback-return
+    menuItems.map((i) => {
+      var addToCartButton = within(i).getByRole("button");
+      expect(addToCartButton).toBeInTheDocument();
+      expect(addToCartButton).toHaveTextContent("Add to Order");
+    });
+  });
+
+  it("Adds a menu item to the cart", async () => {
+    const user = userEvent.setup();
+    render(<HomePage></HomePage>);
+
+    const menuList = screen.getByTestId("menuList");
+    const menuItems = within(menuList).getAllByRole("listitem");
+    const addToCartButton = within(menuItems[0]).getByRole("button");
+
+    await user.click(addToCartButton);
+
+    const cart = screen.getByTestId("cart");
+    expect(within(cart).getByText(foodMenu[0])).toBeInTheDocument();
+
+    const placeOrderButton = within(cart).getByRole("button");
+    expect(placeOrderButton).toBeEnabled();
   });
 });
--- codeoven/src/pages/HomePage.tsx
+++ codeoven/src/pages/HomePage.tsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import { foodMenu } from "../data/MenuItems";
 
 export const HomePage = () => {
+  const [cart, setCart] = useState<string[]>([]);
+
+  const addToCart = (item: string) => {
+    setCart((alreadyAddedItems) => [...alreadyAddedItems, item]);
+  };
+
   return (
     <>
       <h1>The Code Oven</h1>
-      <ul>
-        {foodMenu.map((i) => (
-          <li key={i}>{i}</li>
-        ))}
-      </ul>
+      <div data-testid="menuList">
+        <ul>
+          {foodMenu.map((i) => (
+            <li key={i}>
+              <div>
+                <p>{i}</p>
+                <button onClick={() => addToCart(i)}>Add to Order</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div data-testid="cart">
+        <ul>
+          {cart.map((i) => (
+            <li key={i}>{i}</li>
+          ))}
+        </ul>
+        <button disabled={cart.length === 0}>Place Order</button>
+      </div>
     </>
   );
 };
